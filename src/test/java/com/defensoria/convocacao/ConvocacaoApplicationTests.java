a29ab51b4e2f6f9fe240b1df945438fb4cab90b6
Leonardson Silva <leonardsonsilva.dit@gmail.com>
2023-11-21 13:04:56 -0400

    initial commit

---
+++ src/test/java/com/defensoria/convocacao/ConvocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defensoria.convocacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
