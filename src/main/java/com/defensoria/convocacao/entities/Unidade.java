c58ea8be5be5ae32a3e0c9f7036bd58dbf0f53cf
Leonardson Silva <leonardsonsilva@defensoria.am.def.br>
2024-04-16 18:33:56 -0400

    build(deps): adds hibernate-envers

--- src/main/java/com/defensoria/convocacao/entities/Unidade.java
+++ src/main/java/com/defensoria/convocacao/entities/Unidade.java
@@ -2,6 +2,8 @@ package com.defensoria.convocacao.entities;
 
 import java.util.UUID;
 
+import org.hibernate.envers.Audited;
+
 import com.defensoria.convocacao.interfaces.UniqueEntityId;
 
 import jakarta.persistence.Entity;
@@ -14,6 +16,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "unidade")
+@Audited
 public class Unidade implements UniqueEntityId {
     private static final long serialVersionUID = 1L;
 
@@ -22,6 +25,8 @@ public class Unidade implements UniqueEntityId {
     private UUID id;
 
     private String nome;
+
+    private String descricao;
 
     @ManyToOne
     @JoinColumn(name = "orgao_id", nullable = false)
@@ -37,6 +42,14 @@ public class Unidade implements UniqueEntityId {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public Orgao getOrgao() {
